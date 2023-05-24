2009b52e03ddb503bb16f38ff9f36ebe0cad4f49
Liulieer <2386691195@qq.com>
2023-05-24 19:50:47 +0800

    更新

--- jinfeng-v1.0/jinfeng-v1.0-Vue2/vue.config.js
+++ jinfeng-v1.0/jinfeng-v1.0-Vue2/vue.config.js
@@ -37,7 +37,10 @@ module.exports = {
     },
     lintOnSave: false, // 关闭格式检查
     productionSourceMap: false, // 打包时不会生成 .map 文件，加快打包速度
-
+    outputDir: "dist",
+    assetsDir: "assets",
+    indexPath: "index.html",
+    publicPath: './',
 }
 
 // let c = 'class';
